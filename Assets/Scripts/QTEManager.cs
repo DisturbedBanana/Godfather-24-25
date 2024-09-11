5a94374cf638d739914f6808dea61fbba6d9bbf1
DisturbedBanana <90250325+DisturbedBanana@users.noreply.github.com>
2024-09-11 09:35:34 +0200

    [EDIT] QTE fixed and finished

--- Assets/Scripts/QTEManager.cs
+++ Assets/Scripts/QTEManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class QTEManager : MonoBehaviour
 {
+    public int[] durations;
     public KeyCode[] validSequenceKeys;
     private KeyCode _chosenSequenceKey;
     private ScaleOverTime _scaleManager;
@@ -16,7 +18,7 @@ public class QTEManager : MonoBehaviour
     {
         _chosenSequenceKey = validSequenceKeys[Random.Range(0, validSequenceKeys.Length)];
         Debug.Log(_chosenSequenceKey.ToString());
-        _scaleManager.StartScaling(QTEduration);
+        _scaleManager.StartScaling(QTEduration, _chosenSequenceKey.ToString());
 
     }
 
@@ -31,11 +33,31 @@ public class QTEManager : MonoBehaviour
                 {
                     _scaleManager.StopScaling(true);
                 }
-                else
+                else if(e.keyCode != _chosenSequenceKey && validSequenceKeys.Contains(e.keyCode))
                 {
                     _scaleManager.StopScaling(false);
                 }
             }
         }
+    }
+
+    private void Update()
+    {
+        if (!_scaleManager.isScaling)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                StartQTE(durations[0]);
+            }
+            else if (Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                StartQTE(durations[1]);
+            }
+            else if (Input.GetKeyDown(KeyCode.Keypad3))
+            {
+                StartQTE(durations[2]);
+            }
+        }
+        
     }
 }
--- Assets/Scripts/ScaleOverTime.cs
+++ Assets/Scripts/ScaleOverTime.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScaleOverTime : MonoBehaviour
 {
+    [SerializeField] private GameObject _qteParent;
+    [SerializeField] private TextMeshPro _qteText;
+
     public Vector3 targetScale;  // Target scale to reach
     public float duration = 1.0f; // Time to reach the target scale
-
     private Vector3 initialScale; // Starting scale of the object
     private float timeElapsed = 0.0f; // Time counter
     public bool isScaling = false; // Scaling flag
@@ -32,7 +35,9 @@ public class ScaleOverTime : MonoBehaviour
             // If the scaling is done, stop updating
             if (t >= 1.0f)
             {
+                SwitchQTEVisibility();
                 isScaling = false;
+                Debug.Log("lose, time out");
             }
         }
     }
@@ -58,16 +63,30 @@ public class ScaleOverTime : MonoBehaviour
             Debug.Log("lose, wrong key");
         }
 
+        SwitchQTEVisibility();
+        _qteText.text = " ";
         isScaling = false;
     }
 
     // Call this method to start the scaling
-    public void StartScaling(float givenDuration)
+    public void StartScaling(float givenDuration, string QTELetter)
     {
+        SwitchQTEVisibility();
         transform.localScale = initialScale;  // Reset the scale to initial
         initialScale = transform.localScale;  // Refresh initial scale if the scale was changed
         timeElapsed = 0.0f;                   // Reset the time
         duration = givenDuration;             // Set the duration
+        _qteText.text = QTELetter;
         isScaling = true;                     // Set the flag to start scaling
+    }
+
+    private void SwitchQTEVisibility()
+    {
+        SpriteRenderer[] renderers = _qteParent.GetComponentsInChildren<SpriteRenderer>();
+
+        foreach (SpriteRenderer renderer in renderers)
+        {
+            renderer.enabled = !renderer.enabled;
+        }
     }
 }
